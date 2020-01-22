bf4f597270b771b4a57e302492235365154e5875
Robert Stuart <rstuart@indue.com.au>
2020-01-22 10:54:37 +1000

    testing removal of .properties file

--- manifests/config.pp
+++ manifests/config.pp
@@ -75,32 +75,33 @@ class qualys_agent::config {
   # For some reason, a .properties file needs to exist on first start, so create it here
   # and keep it present, but don't restart the service if it changes.  Just restart if the
   # *actual* config changes.
-  file { 'qualys_properties':
-    ensure    => $ensure,
-    content   => epp('qualys_agent/qualys-cloud-agent.conf.epp', {
-      activation_id        => $qualys_agent::activation_id,
-      cmd_max_timeout      => $qualys_agent::cmd_max_timeout,
-      cmd_stdout_size      => $qualys_agent::cmd_stdout_size,
-      customer_id          => $qualys_agent::customer_id,
-      hostid_search_dir    => $qualys_agent::hostid_search_dir,
-      log_file_dir         => $qualys_agent::log_file_dir,
-      log_level            => $qualys_agent::log_level,
-      process_priority     => $qualys_agent::process_priority,
-      request_timeout      => $qualys_agent::request_timeout,
-      sudo_command         => $qualys_agent::sudo_command,
-      sudo_user            => $qualys_agent::sudo_user,
-      use_audit_dispatcher => $qualys_agent::use_audit_dispatcher,
-      use_sudo             => $qualys_agent::use_sudo,
-      user                 => $qualys_agent::agent_user,
-      user_group           => $qualys_agent::agent_group,
-    }),
-    mode      => '0640',
-    path      => "${qualys_agent::conf_dir}/qualys-cloud-agent.properties",
-    owner     => $qualys_agent::owner,
-    group     => $qualys_agent::group,
-    show_diff => true,
-    require   => $requires,
-  }
+
+  #file { 'qualys_properties':
+  #  ensure    => $ensure,
+  #  content   => epp('qualys_agent/qualys-cloud-agent.conf.epp', {
+  #    activation_id        => $qualys_agent::activation_id,
+  #    cmd_max_timeout      => $qualys_agent::cmd_max_timeout,
+  #    cmd_stdout_size      => $qualys_agent::cmd_stdout_size,
+  #    customer_id          => $qualys_agent::customer_id,
+  #    hostid_search_dir    => $qualys_agent::hostid_search_dir,
+  #    log_file_dir         => $qualys_agent::log_file_dir,
+  #    log_level            => $qualys_agent::log_level,
+  #    process_priority     => $qualys_agent::process_priority,
+  #    request_timeout      => $qualys_agent::request_timeout,
+  #    sudo_command         => $qualys_agent::sudo_command,
+  #    sudo_user            => $qualys_agent::sudo_user,
+  #    use_audit_dispatcher => $qualys_agent::use_audit_dispatcher,
+  #    use_sudo             => $qualys_agent::use_sudo,
+  #    user                 => $qualys_agent::agent_user,
+  #    user_group           => $qualys_agent::agent_group,
+  #  }),
+  #  mode      => '0640',
+  #  path      => "${qualys_agent::conf_dir}/qualys-cloud-agent.properties",
+  #  owner     => $qualys_agent::owner,
+  #  group     => $qualys_agent::group,
+  #  show_diff => true,
+  #  require   => $requires,
+  #}
 
   file {  'qualys_hostid':
     ensure  => $ensure,
